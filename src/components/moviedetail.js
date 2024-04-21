b2af9224fa1e0894dddd587bd37f332257455b49
Tristen <tristen.perez@ucdenver.edu>
2024-04-21 14:14:07 -0600

    Update

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -8,9 +8,12 @@ import { Image } from 'react-bootstrap';
 class MovieDetail extends Component {
 
     componentDidMount() {
-        const {dispatch} = this.props;
-        if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.movieId));
+        const {dispatch, movieId} = this.props;
+        if (this.props.selectedMovie) {
+            dispatch(fetchMovie(movieId)).catch(error => {
+                console.error("Failed to fetch movie details", error);
+
+            });
         }
     }
 
