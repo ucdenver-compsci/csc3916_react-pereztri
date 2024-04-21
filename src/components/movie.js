48d24fcfa7153fdd2343ff6231b206d99897be81
Tristen <tristen.perez@ucdenver.edu>
2024-04-21 02:37:32 -0600

    Updated movie.js

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,7 +1,7 @@
-import React, { Component, useState } from 'react';
-import {connect} from 'react-redux';
-import {Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
-import { Image } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import { withRouter } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
@@ -61,27 +61,29 @@ class Movie extends Component {
     }
 }
 
-// const mapStateToProps = (state, ownProps) => {
-//     console.log(ownProps);
-//     return {
-//         selectedMovie: state.movie.selectedMovie, 
-//         movieId: ownProps.match.params.movieId
-//     }
-// }
-
-// export default withRouter(connect(mapStateToProps)(Movie));
-
-function Movie(props) {
-    const [selectedMovie] = useState(props.selectedMovie);
-    const params = useParams();
-    const movieId = params.movieId;
-    console.log(movieId);
-    const dispatch = useDispatch();
-    if (selectedMovie == null) {
-        dispatch(fetchMovie(movieId));
+const mapStateToProps = (state, ownProps) => {
+    // console.log(ownProps);
+    const movieId = ownProps.match.params.movieId;
+    return {
+        selectedMovie: state.movie.selectedMovie, 
+        movieId: movieId
+        // movieId: ownProps.match.params.movieId
     }
-
-    return (<MovieDetail movieId={movieId} />)
 }
 
-export default Movie;
+export default withRouter(connect(mapStateToProps)(Movie));
+
+// function Movie(props) {
+//     const [selectedMovie] = useState(props.selectedMovie);
+//     const params = useParams();
+//     const movieId = params.movieId;
+//     console.log(movieId);
+//     const dispatch = useDispatch();
+//     if (selectedMovie == null) {
+//         dispatch(fetchMovie(movieId));
+//     }
+
+//     return (<MovieDetail movieId={movieId} />)
+// }
+
+// export default Movie;
